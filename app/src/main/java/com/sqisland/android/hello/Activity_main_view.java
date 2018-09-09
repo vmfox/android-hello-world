6742e598f8c12828a86488d51ade5249a13cbf69
vmfox <vitaliy.martynets@hotmail.com>
2018-09-09 19:02:24 +0100

    Added main screen.

---
+++ app/src/main/java/com/sqisland/android/hello/Activity_main_view.java
@@ -0,0 +1,14 @@
+package com.sqisland.android.hello;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_main_view extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view_l);
+    }
+}
